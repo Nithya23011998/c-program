9b6c4445d5624a51b5096a3c485e78a6e0ef86fa
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2017-08-08 16:54:40 +0530

    vowel.c
    checks given character is alphabet or not

--- vowel.c
+++ vowel.c
@@ -4,8 +4,7 @@ void main()
 char character ;
 printf("enter a character:");
 scanf("%s",character);
-if(character=='a' || character=='A' || character=='e' || character=='E' || character='i' || characte=r='I' || character=='o' || character=='O' ||
-character=='u' || character=='U')
+if(character=='a' || character=='A' || character=='e' || character=='E' || character='i' || characte=r='I' || character=='o' || character=='O' ||character=='u' || character=='U')
 {
 printf("vowel");
 }
