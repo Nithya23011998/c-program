0dceef5097f4c32db7b43e964c4f2443ae496948
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2017-08-22 15:07:21 +0530

    leap.c
    to show the given year is leap year or not

--- leap.c
+++ leap.c
@@ -6,6 +6,17 @@ printf(enter the year);
 scanf("%d",&year);
 if(year%4==0)
 {
+  if(year%100 == 0)
+  {
+    if(year%400 == 0)
+    {
+      printf(""%d is a leap year");
+     }
+ else
+             {
+               printf("%d is not a leap year");
+             }
+
 printf("The given year is a leap year");
 }
 else
