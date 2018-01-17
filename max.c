8bbe0df867bed213c9f12f8629cc8e5bbb19a07b
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-01-17 11:28:53 +0000

    max.c
    maximum number

---
+++ max.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int n=5,sum=0,c,val;
+	int max=0;
+	printf("enter the numbers:");
+	for(c=1;c<=n;c++)
+	{
+		scanf("%d",&val);
+		if(val<max,c<=n)
+		
+			max=val;
+		
+		
+	else
+	max=val;
+	
+	}
+	printf("maximum:%d",max);
+}
