15b01b8949fad35a85aaf60df75d44af3e13df5b
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-02-02 11:04:02 +0000

    length.c
    gives length of a string

---
+++ length.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+char a[50];
+int l=0,i;
+printf("\nEnter the string:");
+scanf("%s",&a[i]);
+printf(" \nlength of a string is :%d",l=strlen(a));
+return 0;
+
+}
