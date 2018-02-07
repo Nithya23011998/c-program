eab155cff5285937f2bb33caae59acc0b9680eba
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-02-07 11:29:27 +0000

    strprint.c
    string prints upto n times

---
+++ strprint.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+    int n,i;
+    char a[10];
+    printf("\n enter the string:");
+    scanf("%s",&a[i]);
+    printf("\n enter the number of times:");
+    scanf("%d",&n);
+    for(i=0;a[i]<n;i++)
+    {
+    	printf("\n %s",a[i]);
+    }
+    	return 0;
+} 
