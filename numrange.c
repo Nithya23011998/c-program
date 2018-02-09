6ce64f6146bf1601b3ab2c01149252441cb33d77
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-02-09 16:45:04 +0530

    numrange.c
    whether the given number is within 1 to 10

---
+++ numrange.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("\n enter the number:");
+	scanf("%d",&n);
+	if(n >=1 && n<= 10)
+	{
+		printf("\n number is eithin the range");
+	}
+	else
+	{
+		printf("\n not in range");
+	}
+	return 0;
+}
