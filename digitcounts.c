e16894e7aaec0a660e2417143f8ef70b78eb925d
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-02-17 20:37:25 +0530

    digitcounts.c
    count the number of digits in a number

---
+++ digitcounts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,count=0;
+	printf("\n enter the number:");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		n=n/10;
+		count++;
+
+	}
+	printf("\n count is:%d",count);
+	return 0;
+}
