467b1aa8c1bb6a58b74f0077219644693375ca7c
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-02-21 15:41:56 +0530

    33.c
    space count

---
+++ 33.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	char a[125];
+	int i,count=0;
+	printf("\n enter the string:");
+	scanf("%s",&a[i]);
+	for(i=0;a[i]<='';i++)
+	{
+		count++;
+	}
+	printf("\n  space count:%d",count);
+	return 0;
+}
