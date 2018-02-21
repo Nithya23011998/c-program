155259b8e13b6a736ae667b168f57311443b5210
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-02-21 16:56:44 +0530

    58swapbit.c
    swap using bitwise bitwise operator

--- 58swapbit.c
+++ 58swapbit.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,temp=0;
-	printf("\n enter the numbers to be swapped:");
+	int a,b,t=0;
+	printf("\n enter the numbers  :");
 	scanf("\n %d,%d",&a,&b);
 	a=a^b;
 	b=b^a;
 	
-	printf("\n the numbers after swapped: %d,%d ",a,b);
+	printf("\n  after swap: %d,%d ",a,b);
 	return 0;
 	
 	
