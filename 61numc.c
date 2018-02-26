df8fde3a264d272d5be75e7cfa658ae17dc989e3
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-02-26 10:29:49 +0000

    61numc.c
    prints character upto n

---
+++ 61numc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	int n,i;
+	char  s[10];
+	printf("\n enter the string:");
+	scanf("%s",s);
+
+	printf("\n enter the number:");
+	scanf("%d",&n);
+	for(s[i]=0;s[i]<=n;++i)
+	{
+		printf("\n %s",s);
+
+	}
+	return 0;
+}
