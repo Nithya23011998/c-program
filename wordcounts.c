482c41a9bada9f71d1b148b2d90a988587e139e1
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-02-26 10:56:14 +0000

    wordcounts.c
    word count

--- wordcounts.c
+++ wordcounts.c
@@ -3,15 +3,15 @@
 int main()
 {
 	char a[100];
-	int c=0,i;
+	int ch=0,i;
 	printf("\enter the sentence:");
 	scanf("%[^\n]s",a);
 	for(i=0 ; a[i] != "\0" ; i++ )
 	{
 		if(a[i]== ' ')
-		c++;
+		ch++;
 		
 	}
-	printf("\n number of words:%d",c+1);
+	printf("\n number of words:%d",ch+1);
 	return 0;
 }
