6f1d50a487aa07d5b331ad457a068292dfdca3a2
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-03-05 16:45:53 +0530

    82rect.c
    area

---
+++ 82rect.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float l,b,area;
+printf("\n enter the length and breadth of a form:");
+scanf("%f%f",&l,&b);
+area=l*b;
+printf("\n area is:%f",area);
+return 0;
+}
