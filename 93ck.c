520e891c6b192a77fa88c89dd1916cece11d3149
Nithya23011998 <30748358+Nithya23011998@users.noreply.github.com>
2018-03-14 11:02:49 +0000

    93ck.c
    celcius to kelvin

---
+++ 93ck.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int cel,kel;
+printf("\n enter the temperatur in celcius:");
+scanf("%d",&cel);
+kel=cel+273;  // 0 cel=273 kel
+printf("\n temperature in kelvin:%d",kel);
+return 0;
+
+}
